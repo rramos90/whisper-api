921b51fe287cc9ab92f9bfacda1f7373fb5c82f8
rramos90 <musica.rafaellaramos@gmail.com>
2025-06-04 09:30:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ from whisper_logic import transcribe_and_summarize
 
 app = Flask(__name__)
 
+# Rota de ping para evitar cold start
+@app.route('/', methods=['GET'])
+def ping():
+    return jsonify({"status": "ok"})
+
+# Rota principal
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
     audio = request.files['file']
